f10035b4037746c269f7b807afa896d77ff8a0a4
askazakov <askazakov@skbkontur.ru>
2017-03-19 15:08:45 +0500

    fix tabs vs. spaces

--- Eval/EvalProgram.cs
+++ Eval/EvalProgram.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-using System.Linq;
 
 namespace EvalTask
 {
-	class EvalProgram
-	{
-		static void Main(string[] args)
-		{
-            Evaluator evaluator = new Evaluator();;
-			string input = Console.In.ReadToEnd();
-		    string output = evaluator.EvalString(input);
-            
-			Console.WriteLine(output);
-		}
-	}
+    internal class EvalProgram
+    {
+        private static void Main(string[] args)
+        {
+            var evaluator = new Evaluator();
+            ;
+            var input = Console.In.ReadToEnd();
+            var output = evaluator.EvalString(input);
+
+            Console.WriteLine(output);
+        }
+    }
 }
