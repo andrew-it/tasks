f9985c4912e2168d4b38bda7bf35b1b01db9b88c
Andrew <a.ermak@innopolis.ru>
2017-03-19 11:30:21 +0500

    simQL1

--- SimQL/SimQLProgram.cs
+++ SimQL/SimQLProgram.cs
@@ -2,27 +2,67 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using FluentAssert;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
 
 namespace SimQLTask
 {
-	class SimQLProgram
-	{
-		static void Main(string[] args)
-		{
-			var json = Console.In.ReadToEnd();
-			foreach (var result in ExecuteQueries(json))
-				Console.WriteLine(result);
-		}
+    internal class SimQLProgram
+    {
+        private static void Main(string[] args)
+        {
+            var json = Console.In.ReadToEnd();
+            foreach (var result in ExecuteQueries(json))
+                Console.WriteLine(result);
+        }
 
-		public static IEnumerable<string> ExecuteQueries(string json)
-		{
-			var jObject = JObject.Parse(json);
-			var data = (JObject)jObject["data"];
-			var queries = jObject["queries"].ToObject<string[]>();
-			// TODO
-			return queries.Select(q => "TODO");
-		}
-	}
+        public static IEnumerable<string> ExecuteQueries(string json)
+        {
+            var jObject = JObject.Parse(json);
+            var data = (JObject) jObject["data"];
+            var queries = jObject["queries"].ToObject<string[]>();
+            return queries.Select(q => GetResultByQuery(q, data));
+        }
+
+        public static string GetResultByQuery(string query, JObject data)
+        {
+            var steps = query.Split('.');
+            var result = data[steps[0]];
+            for (int i = 1; i < steps.Length; i++)
+            {
+                result = result[steps[i]];
+            }
+            return $"{query} = {result.ToString(Formatting.Indented)}";
+        }
+    }
+
+    [TestFixture]
+    public class SimQLTester
+    {
+        private const string jsonData = @"{
+                                                'data': {
+                                                    'a': {
+                                                        'x':3.14, 
+                                                        'b': {'c':15}, 
+                                                        'c': {'c':9}
+                                                    }, 
+                                                    'z':42
+                                                },
+                                                'queries': [
+                                                    'a.b.c',
+                                                    'z',
+                                                    'a.x'
+                                                ]
+                                            }";
+       
+    [TestCase(jsonData, new[] { "a.b.c = 15", "z = 42", "a.x = 3.14" })]
+        public void GetValue_ByQuery(string query, IEnumerable<string> result)
+        {
+            SimQLProgram.ExecuteQueries(query)
+                .ShouldBeEqualTo(result);
+        }
+    }
 }
